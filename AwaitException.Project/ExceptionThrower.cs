43224337d68bebe95473788f9795fd2602da9503
david <ariedov@gmail.com>
2019-03-31 16:35:35 +0300

    initial commit.
    adds an experiment that shows that non-awaited method exception will not crash the app

---
+++ AwaitException.Project/ExceptionThrower.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+
+namespace Project
+{
+    public class ExceptionThrower
+    {
+        public async Task DelayAndThrow(int sleepForMilliseconds)
+        {
+            await Task.Delay(sleepForMilliseconds);
+
+            throw new AwaitException();
+        }
+    }
+}
---
+++ AwaitExpception.Test/Tests.cs
@@ -0,0 +1,32 @@
+﻿using System.Threading.Tasks;
+using NUnit.Framework;
+using Project;
+
+namespace Test
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void TestAwaitExceptionThrown()
+        {
+            var thrower = new ExceptionThrower();
+            // the exception is thrown when we await
+            Assert.ThrowsAsync<AwaitException>(async () => await thrower.DelayAndThrow(5));
+        }
+
+        [Test]
+        public async Task TestFreeFallExceptionThrow()
+        {
+            var thrower = new ExceptionThrower();
+
+#pragma warning disable 4014
+            // we throw the exception and not awaiting the method
+            // the exception gets lost in the universe
+            thrower.DelayAndThrow(5);
+
+            // wait for the test to finish
+            await Task.Delay(10);
+        }
+    }
+}
